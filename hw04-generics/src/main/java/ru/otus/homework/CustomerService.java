ff74b64cdb7042e618a7f886784215bda282b1f3
Dmitriy Fedotov <dfedotov@usetech.ru>
2023-03-19 13:00:05 +0300

    [hw02-generics] Применение коллекций CustomerService использовал TreeMap

--- hw04-generics/src/main/java/ru/otus/homework/CustomerService.java
+++ hw04-generics/src/main/java/ru/otus/homework/CustomerService.java
@@ -4,35 +4,32 @@ package ru.otus.homework;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
+import java.util.TreeMap;
 
 @Slf4j
 public class CustomerService {
 
-    //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
-    private final Map<Customer, String> map = new HashMap<>();
+    private final TreeMap<Customer, String> treeMap = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
 
     public Map.Entry<Customer, String> getSmallest() {
         log.info("getSmallest");
         //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        var optionalEntry = map.entrySet().stream()
-                .min(Map.Entry.comparingByKey(Comparator.comparing(Customer::getScores)));
-        return optionalEntry.map(c -> Map.entry(new Customer(c.getKey()), c.getValue())).orElse(null);
+        return Map.entry(new Customer(treeMap.firstKey()), treeMap.get(treeMap.firstKey()));
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
         log.info("getNext Customer [{}]", customer);
-        var optionalEntry = map.entrySet().stream()
+        var optionalEntry = treeMap.entrySet().stream()
                 .filter(m -> m.getKey().getScores() > customer.getScores())
-                .min(Map.Entry.comparingByKey(Comparator.comparing(Customer::getScores)));
+//                .min(Map.Entry.comparingByKey(Comparator.comparingLong(Customer::getScores)));
+                .findFirst();
         return optionalEntry.map(c -> Map.entry(new Customer(c.getKey()), c.getValue())).orElse(null);
     }
 
     public void add(Customer customer, String data) {
         log.info("add Customer [{}], data [{}]", customer, data);
-        map.put(customer, data);
+        treeMap.put(customer, data);
     }
 }
