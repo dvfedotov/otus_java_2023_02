6e45d7adb95984144146fbda2c99a526f01b766e
Dmitriy Fedotov <dfedotov@usetech.ru>
2023-03-19 13:31:41 +0300

    [hw02-generics] Применение коллекций CustomerReverseOrder использовал ArrayDeque

--- hw04-generics/src/main/java/ru/otus/homework/CustomerReverseOrder.java
+++ hw04-generics/src/main/java/ru/otus/homework/CustomerReverseOrder.java
@@ -2,17 +2,20 @@ package ru.otus.homework;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayDeque;
+
 @Slf4j
 public class CustomerReverseOrder {
 
-    //todo: 2. надо реализовать методы этого класса
     //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
+    private final ArrayDeque<Customer> queue = new ArrayDeque<>();
 
     public void add(Customer customer) {
+        queue.add(customer);
 
     }
 
     public Customer take() {
-        return null; // это "заглушка, чтобы скомилировать"
+        return queue.pollLast();
     }
 }
