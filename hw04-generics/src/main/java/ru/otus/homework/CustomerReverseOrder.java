c88a173b5f4aecd7c2784cf2b4aa5be4210e1a0f
Dmitriy Fedotov <dfedotov@usetech.ru>
2023-03-21 10:22:40 +0300

    [hw02-generics] Доработал CustomerReverseOrder - Слева лучше писать интерфейс

--- hw04-generics/src/main/java/ru/otus/homework/CustomerReverseOrder.java
+++ hw04-generics/src/main/java/ru/otus/homework/CustomerReverseOrder.java
@@ -1,12 +1,13 @@
 package ru.otus.homework;
 
 import java.util.ArrayDeque;
+import java.util.Deque;
 
 
 public class CustomerReverseOrder {
 
     //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
-    private final ArrayDeque<Customer> queue = new ArrayDeque<>();
+    private final Deque<Customer> queue = new ArrayDeque<>();
 
     public void add(Customer customer) {
         queue.add(customer);
