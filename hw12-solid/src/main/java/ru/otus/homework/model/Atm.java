d7dd53b9fc3e3cb32358e1053aa296697f1cfa42
Dmitriy Fedotov <dfedotov@usetech.ru>
2023-05-01 20:10:01 +0300

    [hw12-solid] У нас же есть более подходящие коллекции, что будут хранить в нужной форме исходно. К тому же система расчитана что под каждый номинал - максимум одна ячейка

--- hw12-solid/src/main/java/ru/otus/homework/model/Atm.java
+++ hw12-solid/src/main/java/ru/otus/homework/model/Atm.java
@@ -6,13 +6,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.CollectionUtils;
 import ru.otus.homework.exception.AtmException;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.EnumMap;
-import java.util.List;
 import java.util.Map;
+import java.util.NavigableSet;
 import java.util.Optional;
+import java.util.Set;
+import java.util.TreeSet;
 
 @Slf4j
 @Getter
@@ -22,54 +22,51 @@ public class Atm {
     public static final String ERROR_MESSAGE_NO_CASH = "Failed to receive the amount because there is no cash in the ATM";
     public static final String ERROR_MESSAGE_NOT_ENOUGH_CASH = "Failed to receive the amount because there is not enough cash in the ATM";
     public static final String ERROR_MESSAGE_NO_BANKNOTES = "Failed to receive the amount because there are no necessary banknotes in the ATM";
-    private List<Cell> cellList;
+    private final Set<Cell> cells;
+
+    public Atm() {
+        NavigableSet<Cell> treeSet = new TreeSet<>(Comparator.comparingInt(c -> c.getCurrency().getValue()));
+        treeSet.add(new Cell(Currency.ONE_HUNDRED));
+        treeSet.add(new Cell(Currency.FIVE_HUNDRED));
+        treeSet.add(new Cell(Currency.ONE_THOUSAND));
+        treeSet.add(new Cell(Currency.FIVE_THOUSAND));
+        cells = treeSet.descendingSet();
+    }
 
     public void addCurrency(Currency currency, int count) {
-        if (CollectionUtils.isEmpty(cellList)) {
-            cellList = new ArrayList<>();
-        }
         Optional<Cell> optionalCell = findCell(currency);
-        if (optionalCell.isPresent()) {
-            Cell cell = optionalCell.get();
-            cell.setCount(cell.getCount() + count);
-        } else {
-            cellList.add(new Cell(currency, count));
-        }
+        Cell cell = optionalCell.get();
+        cell.addCash(count);
     }
 
     public int getBalance() {
-        if (CollectionUtils.isEmpty(cellList)) {
+        if (CollectionUtils.isEmpty(cells)) {
             return 0;
         }
-        return cellList.stream()
+        return cells.stream()
                 .mapToInt(c -> c.getCurrency().getValue() * c.getCount())
                 .reduce(0, Integer::sum);
     }
 
     public Map<Currency, Integer> getCash(int sum) {
-        if (CollectionUtils.isEmpty(cellList)) {
+        int balance = getBalance();
+        if (balance <= 0) {
             throw new AtmException(ERROR_MESSAGE_NO_CASH);
         }
-        if (sum > getBalance()) {
+        if (sum > balance) {
             throw new AtmException(ERROR_MESSAGE_NOT_ENOUGH_CASH);
         }
-        reverseSortCellList();
         Map<Currency, Integer> cashMap = getCashMap(sum);
         updateCellList(cashMap);
         return cashMap;
     }
 
-    private void reverseSortCellList() {
-        cellList.sort(Comparator.comparingInt(c -> c.getCurrency().getValue()));
-        Collections.reverse(cellList);
-    }
-
     private Map<Currency, Integer> getCashMap(int sum) {
         Map<Currency, Integer> cashMap = new EnumMap<>(Currency.class);
-        for (Cell cell : cellList) {
+        for (Cell cell : cells) {
             int currency = cell.getCurrency().getValue();
             int count = sum / currency;
-            if (count > 0) {
+            if (cell.getCount() > 0 && count > 0) {
                 if (cell.getCount() >= count) {
                     cashMap.put(cell.getCurrency(), count);
                     sum = sum - currency * count;
@@ -88,18 +85,11 @@ public class Atm {
     private void updateCellList(Map<Currency, Integer> cashMap) {
         for (Map.Entry<Currency, Integer> entry : cashMap.entrySet()) {
             Optional<Cell> optionalCell = findCell(entry.getKey());
-            optionalCell.ifPresent(cell -> {
-                int count = cell.getCount() - entry.getValue();
-                if (count <= 0) {
-                    cellList.remove(cell);
-                } else {
-                    cell.setCount(count);
-                }
-            });
+            optionalCell.ifPresent(c -> c.getCash(entry.getValue()));
         }
     }
 
     private Optional<Cell> findCell(Currency currency) {
-        return cellList.stream().filter(c -> c.getCurrency().equals(currency)).findFirst();
+        return this.cells.stream().filter(c -> c.getCurrency().equals(currency)).findFirst();
     }
 }
