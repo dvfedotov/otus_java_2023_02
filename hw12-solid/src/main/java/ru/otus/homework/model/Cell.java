3869c3ecbfc170e2a3844307324680fe1c50b574
Dmitriy Fedotov <dfedotov@usetech.ru>
2023-05-01 20:09:24 +0300

    [hw12-solid] Сложно представить что ячейка для купюр в 100 одним движением превратится в ячейку с тысячным. Сейчас система от этого не защищена

--- hw12-solid/src/main/java/ru/otus/homework/model/Cell.java
+++ hw12-solid/src/main/java/ru/otus/homework/model/Cell.java
@@ -2,15 +2,30 @@ package ru.otus.homework.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
 @Getter
-@Setter
 @ToString
 @AllArgsConstructor
 public class Cell {
 
     private Currency currency;
     private int count;
+
+    public Cell(Currency currency) {
+        this.currency = currency;
+    }
+
+    public void addCash(int count) {
+        if (count > 0) {
+            this.count += count;
+        }
+    }
+
+    public void getCash(int count){
+        if(count >= this.count){
+            this.count-=count;
+        }
+    }
+
 }
